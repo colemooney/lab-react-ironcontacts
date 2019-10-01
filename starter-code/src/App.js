7685029c3b52d7d5ed3b8e2912cf055feb964395
Cole Mooney <colemooney@Coles-MacBook-Pro.local>
2019-10-01 14:24:32 -0400

    iteration 3

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import contacts from './contacts.json'
 
 class App extends Component {
+  state = {
+    celebs: contacts.splice(0,5)
+  }
+ 
+  
+  
+    loopNames = () =>{
+      let actors = this.state.celebs.map((eachActor)=>{
+        return(
+          <tr>
+          <td><img src={eachActor.pictureUrl} width="100" alt="" /></td>
+          <td>{eachActor.name}</td>
+          <td>{eachActor.popularity}</td>
+          </tr>
+        )
+      })
+
+      
+return actors
+    
+  };
+
+  randomCeleb = () =>{
+    let allCelebs = [...this.state.celebs]
+ let randomIndex = Math.floor(Math.random()*(allCelebs.length))
+ allCelebs.push(contacts[randomIndex])
+ this.setState({
+     celebs: allCelebs
+ })
+    
+  }
+
+  sortName = () =>{
+    
+  }
+
   render() {
+
+
+  
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Iron Contacts</h1>
+
+        <button onClick={this.randomCeleb} >Add Random Contact</button>
+        <button onClick={this.randomCeleb} >Sort By Name</button>
+        <button onClick={this.randomCeleb} >Sort By Popularity</button>
+        
+        <table> 
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        {this.loopNames()}
+        </table>
+        
       </div>
     );
   }
